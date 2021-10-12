ab3e65ae3a422e55dfb78bdee20b8402f19da3df
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-12 21:04:01 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Sergey Lobanov on 12.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
